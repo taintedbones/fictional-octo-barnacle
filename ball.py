a6592d3d0f73641b53752fc0fa5035fff0c29ea9
taintedbones <kierstyn.erin@gmail.com>
2019-09-19 00:13:27 -0700

    Das pumpkin head moves

--- ball.py
+++ ball.py
@@ -1,63 +1,45 @@
 import pygame
 import random
-from pygame.sprite import Sprite
+from pygame.math import Vector2
 
+BLACK = (0, 0, 0)
 ball_image = pygame.image.load('pumpkin.jpg')
 
 
-class Ball(Sprite):
-    def __init__(self, surface, start_speed, acceleration):
-        # Calls the base class, same as pygame.sprite.Sprite.__init__(self)
-        super(Ball, self).__init__()
-        self.surface = surface
-        self.surface_rect = surface.get_rect()
+def vector2(xy_tuple, scale):
+    v = Vector2()
+    v[0], v[1] = xy_tuple[0], xy_tuple[1]
+    return v * scale
 
-        self.rect = pygame.Rect(0, 0, 20, 20)
-        self.rect.center = self.surface_rect.center
 
-        self.ball_start_speed = 10
-        self.ball_acceleration = 5
-        self.ball_angular
-        # ball hit sound
+class Ball:
+    def __init__(self, x, y, width, height, velocity, surface):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.velocity = 10
+        self.surface = surface;
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.move_x = velocity[0]
+        self.move_y = velocity[1]
 
-        rand_vel = random.uniform(-self.ball_start_speed, self.ball_start_speed)
-        self.velocity_x = rand_vel
-        self.velocity_y = self.ball_start_speed - abs(rand_vel)
+    def draw(self):
+        pygame.draw.rect(self.surface, BLACK, self.rect)
+        self.surface.blit(ball_image, (self.x, self.y))
 
-        self.last_paddle_collided_with = None
+    def move(self):
+        self.x -= (self.velocity * (self.move_x / 10))
+        self.y -= (self.velocity * (self.move_y / 10))
+        self.rect.topleft = self.x, self.y
 
-    def calculate_horizontal_collision(self, collision):
-        if collision:
-            if collision != self.last_paddle_collided_with:
-                self.last_paddle_collided_with = collision
-                self.velocity_x *= -1
-                difference = self.rect.centery - collision.rect.centery
-                velocity_change_amount = difference * self.ball_angular
-                self.velocity_y += velocity_change_amount
+    def collision(self, h_paddles, v_paddles):
+        r = self.rect
 
-    def calculate_vertical_collision(self):
-        if collision:
-            if collision != self.last_paddle_collided_with:
-                self.last_paddle_collided_with = collision
-                self.velocity_y *= -1
-                difference = self.rect.centery - collision.rect.centery
-                velocity_change_amount = difference * self.ball_angular
-                self.velocity_x += velocity_change_amount
+        if r.colliderect(h_paddles[0]) or r.colliderect(h_paddles[1]):
+            self.move_x *= -1
+            self.velocity += 0.2
 
-    def update(self, paddles):
-        self.velocity_x += self.ball_acceleration if self.velocity_x > 0 else -self.ball_acceleration
-        self.center_x += self.velocity_x
-        self.rect.centerx = self.center_x
-
-        collision = pygame.sprite.spritecollideany(self, paddles)
-        self.calcutate_horizontal_collision(collision)
-        self.velocity_y += self.ball_acceleration if self.velocity_y > 0 else -self.ball_acceleration
-        self.center_y += self.velocity_y
-        self.rect.centery = self.center_y
-
-        collision = pygame.sprite.spritecollideany(self, paddles)
-        self.calculate_vertical_collision(collision)
-
-    def draw(self, color):
-
-        pygame.draw.Rect(self.surface, color, self.rect)
+        if r.colliderect(v_paddles[0]) or r.colliderect(v_paddles[1]) or r.colliderect(v_paddles[2]):
+            self.move_y *= -1
+            self.velocity += 0.2
--- pong.py
+++ pong.py
@@ -3,7 +3,7 @@ import sys
 import random
 from pygame.locals import *
 from paddle import *
-import ball
+from ball import *
 
 # Set up window
 WINDOWWIDTH = 800
@@ -23,6 +23,9 @@ pygame.init()
 
 window_surface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
 pygame.display.set_caption('Pong No Wallzz')
+
+ball = Ball(WINDOWWIDTH / 2, WINDOWHEIGHT / 2, 20, 20, (0, 2), window_surface)
+
 p_top_paddle = HorizontalPaddle(90, 20, 100, 10, window_surface)
 p_bottom_paddle = HorizontalPaddle(90, WINDOWHEIGHT - 90, 100, 10, window_surface)
 p_side_paddle = VerticalPaddle(20, 130, 10, 100, window_surface)
@@ -49,7 +52,7 @@ def draw_score(surface, text, x, y):
     surface.blit(text_render, text_rect)
 
 
-def initialize_screen():
+def update_screen():
     window_surface.fill(BLACK)
     draw_dashed_line()
     draw_score(window_surface, 'Player: ', 30, WINDOWHEIGHT - 60)
@@ -59,11 +62,14 @@ def initialize_screen():
     p_side_paddle.draw()
     p_bottom_paddle.draw()
 
+    ball.draw()
+    ball.move()
+
     pygame.display.update()
 
 
 def play_game():
-    initialize_screen()
+    update_screen()
 
     # Runs game loop
     while True:
@@ -76,35 +82,20 @@ def play_game():
                     if p_top_paddle.x > 0:
                         p_top_paddle.move_left()
                         p_bottom_paddle.move_left()
-                if event.key == K_RIGHT or event.key == K_d:
+                elif event.key == K_RIGHT or event.key == K_d:
                     if p_top_paddle.x < WINDOWWIDTH:
                         p_top_paddle.move_right()
                         p_bottom_paddle.move_right()
-                if event.key == K_UP or event.key == K_w:
+                elif event.key == K_UP or event.key == K_w:
                     if p_side_paddle.y > 0:
                         p_side_paddle.move_up()
-                if event.key == K_DOWN or event.key == K_s:
+                elif event.key == K_DOWN or event.key == K_s:
                     if p_side_paddle.y < WINDOWHEIGHT - p_side_paddle.height:
                         p_side_paddle.move_down()
-                if event.key == K_ESCAPE:
+                elif event.key == K_ESCAPE:
                     pygame.quit()
-                    sys.exit()
-            # if event.type == KEYUP:
-            #     if event.key == K_ESCAPE:
-            #         pygame.quit()
-            #         sys.exit()
-            #     if event.key == K_LEFT or event.key == K_a:
-            #         p_top_paddle.move_left()
-            #         p_bottom_paddle.move_left()
-            #     if event.key == K_RIGHT or event.key == K_d:
-            #         p_top_paddle.move_right()
-            #         p_bottom_paddle.move_right()
-            #     if event.key == K_UP or event.key == K_w:
-            #         p_side_paddle.move_up()
-            #     if event.key == K_DOWN or event.key == K_s:
-            #         p_side_paddle.move_down()
 
-        initialize_screen()
+        update_screen()
         pygame.display.update()
 
 
